07da462babe56a6f4a220c12afa1bf0a3d29b448
Office-HRR <727870282@qq.com>
2020-04-11 11:01:44 +0800

    1.init; 2.仿爱奇艺客户端播放暂停按钮动画

---
+++ app/src/main/java/com/test/customview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.test.customview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.test.customview.view.PlayAndPauseView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button start, reverse;
+    private PlayAndPauseView animationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start =  findViewById(R.id.start);
+        reverse = findViewById(R.id.reverse);
+        animationView = findViewById(R.id.playAndPauseView);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View param1View) {
+                animationView.animationStart();
+            }
+        });
+        reverse.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View param1View) {
+                animationView.animationReverse();
+            }
+        });
+    }
+}
+
